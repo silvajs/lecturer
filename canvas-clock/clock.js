75211abaa93bcc4537294a29c4c6726f351201e8
Silva <zhoulinspq2006@126.com>
2016-02-28 23:00:02 +0800

    finish

--- canvas-clock/clock.js
+++ canvas-clock/clock.js
@@ -3,37 +3,38 @@ var ctx = dom.getContext('2d');
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 var r = width / 2;
+var rem = width / 200;
 
 function drawBackground() {
 	ctx.save();
 	ctx.translate(r, r);
 	ctx.beginPath();
-	ctx.lineWidth = 10;
-	ctx.arc(0, 0, r - 5, 0, 2 * Math.PI, false);
+	ctx.lineWidth = 10 * rem;
+	ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);
 	ctx.stroke();
 
 	var hourNumbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
-	ctx.font = '18px Arial';
+	ctx.font = 18 * rem + 'px Arial';
 	ctx.textAlign = 'center';
 	ctx.textBaseline = 'middle';
 	hourNumbers.forEach(function(number, i) {
 		var rad = 2 * Math.PI / 12 * i;
-		var x = Math.cos(rad) * (r - 30);
-		var y = Math.sin(rad) * (r - 30);
+		var x = Math.cos(rad) * (r - 30 * rem);
+		var y = Math.sin(rad) * (r - 30 * rem);
 		ctx.fillText(number, x, y);
 	});
 
 	for (var i = 0; i < 60; i++) {
 		var rad = 2 * Math.PI / 60 * i;
-		var x = Math.cos(rad) * (r - 18);
-		var y = Math.sin(rad) * (r - 18);
+		var x = Math.cos(rad) * (r - 18 * rem);
+		var y = Math.sin(rad) * (r - 18 * rem);
 		ctx.beginPath();
 		if (i % 5 === 0) {
 			ctx.fillStyle = '#000';
-			ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+			ctx.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
 		} else {
 			ctx.fillStyle = '#ccc';
-			ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+			ctx.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
 		}
 		ctx.fill();
 	}
@@ -45,9 +46,9 @@ function drawHour(hour, minute) {
 	var rad = 2 * Math.PI / 12 * hour;
 	var mrad = 2 * Math.PI / 12 / 60 * minute;
 	ctx.rotate(rad + mrad);
-	ctx.lineWidth = 6;
+	ctx.lineWidth = 6 * rem;
 	ctx.lineCap = 'round';
-	ctx.moveTo(0, 10);
+	ctx.moveTo(0, 10 * rem);
 	ctx.lineTo(0, -r / 2);
 	ctx.stroke();
 	ctx.restore();
@@ -58,10 +59,10 @@ function drawMinute(minute) {
 	ctx.beginPath();
 	var rad = 2 * Math.PI / 60 * minute;
 	ctx.rotate(rad);
-	ctx.lineWidth = 3;
+	ctx.lineWidth = 3 * rem;
 	ctx.lineCap = 'round';
-	ctx.moveTo(0, 10);
-	ctx.lineTo(0, -r + 30);
+	ctx.moveTo(0, 10 * rem);
+	ctx.lineTo(0, -r + 30 * rem);
 	ctx.stroke();
 	ctx.restore();
 }
@@ -72,10 +73,10 @@ function drawSecond(second) {
 	ctx.fillStyle = '#c14543';
 	var rad = 2 * Math.PI / 60 * second;
 	ctx.rotate(rad);
-	ctx.moveTo(-2, 20);
-	ctx.lineTo(2, 20);
-	ctx.lineTo(1, -r + 18);
-	ctx.lineTo(-1, -r + 18);
+	ctx.moveTo(-2 * rem, 20 * rem);
+	ctx.lineTo(2 * rem, 20 * rem);
+	ctx.lineTo(1, -r + 18 * rem);
+	ctx.lineTo(-1, -r + 18 * rem);
 	ctx.fill();
 	ctx.restore();
 }
@@ -83,7 +84,7 @@ function drawSecond(second) {
 function drawDot() {
 	ctx.beginPath();
 	ctx.fillStyle = '#fff';
-	ctx.arc(0, 0, 3, 0, 2 * Math.PI, false);
+	ctx.arc(0, 0, 3 * rem, 0, 2 * Math.PI, false);
 	ctx.fill();
 }
 
