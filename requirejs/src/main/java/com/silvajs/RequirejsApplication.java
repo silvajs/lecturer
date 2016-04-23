bcdd2330f748298753c4f3017c6beefb2539d731
Silva <zhoulinspq2006@126.com>
2016-04-23 13:02:42 +0800

    sprint boot

---
+++ requirejs/src/main/java/com/silvajs/RequirejsApplication.java
@@ -0,0 +1,12 @@
+package com.silvajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirejsApplication.class, args);
+	}
+}
---
+++ requirejs/src/test/java/com/silvajs/RequirejsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.silvajs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RequirejsApplication.class)
+@WebAppConfiguration
+public class RequirejsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
