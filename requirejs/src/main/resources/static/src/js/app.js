4e5677af611dbbfebedc867cd900a3500cb02f89
Silva <zhoulinspq2006@126.com>
2016-05-17 22:30:03 +0800

    配置不支持amd

--- requirejs/src/main/resources/static/src/js/app.js
+++ requirejs/src/main/resources/static/src/js/app.js
@@ -1,11 +1,23 @@
 requirejs.config({
 	baseUrl: '/js',
 	paths: {
-		'jquery': './lib/jquery'
+		'jquery': './lib/jquery',
+		'bootstrap': './lib/bootstrap',
+		'modernizr': './lib/modernizr',
+		'backbone': './lib/backbone',
+		'underscore': './lib/underscore'
+	},
+	shim: {
+		'modernizr': {
+			exports: 'Modernizr'
+		},
+		'bootstrap': ['jquery']
 	}
 });
 
-require(['jquery', './app/api'], function($, api) {
+require(['jquery', './app/api', 'modernizr', 'backbone', 'bootstrap'], 
+		function($, api, modernizr, Backbone) {
+	console.log(Backbone);
 	$('#user').click(function() {
 		api.getUser().then(function(user) {
 			console.log(user);
